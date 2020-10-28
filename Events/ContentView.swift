6dcee571d746bfac10a91fd08f649b54bc4a63ec
willian-antunes7 <willian.antunes@me.com>
2020-10-28 00:17:41 -0300

    Initial Commit

---
+++ Events/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Events
+//
+//  Created by Willian Antunes on 28/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Events/EventsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventsApp.swift
+//  Events
+//
+//  Created by Willian Antunes on 28/10/20.
+//
+
+import SwiftUI
+
+@main
+struct EventsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
